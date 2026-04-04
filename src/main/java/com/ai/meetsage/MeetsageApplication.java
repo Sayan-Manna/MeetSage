8e87f334bb67b302b12a57ab6a2182d130bdbcb9
Sayan Manna <rexsayan028@gmail.com>
2026-04-05 04:01:11 +0530

    Project setup

---
+++ src/main/java/com/ai/meetsage/MeetsageApplication.java
@@ -0,0 +1,13 @@
+package com.ai.meetsage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeetsageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeetsageApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ai/meetsage/MeetsageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ai.meetsage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeetsageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
